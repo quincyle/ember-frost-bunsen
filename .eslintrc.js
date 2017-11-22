4cb117a98be2ab45c4a4369fa1f5516073095d1e
Quincy Le <lexuqing8@gmail.com>
2017-11-22 15:38:21 -0500

    'updateConfigurations'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  extends: 'frost-standard',
-  rules: {
-    "no-template-curly-in-string": "off",
-    "ocd/sort-import-declarations": [
-      2,
-      {
-        localPrefixes: [
-          "../",
-          "./",
-          "dummy/",
-          "ember-frost-bunsen"
-        ]
-      }
-    ]
-  }
+  extends: 'frost-standard'
 }
